12a7b75258c0cd4ad487414c7978010131edabe5
kashwin M S <kaswin620@gmail.com>
2026-09-16 20:27:45 +0530

    Add apb_agent_config class definition

--- TB/agent_config.sv
+++ TB/agent_config.sv
@@ -1,1 +1,22 @@
 
+// `include "uvm_macros.svh"
+// import uvm_pkg::*;
+  
+  class apb_agent_config extends uvm_object;
+    
+    virtual apb_if vif;
+    
+    `uvm_object_utils(apb_agent_config)
+    
+//     bit has_driver=1;
+//     bit has_monitor=1;
+    
+    uvm_active_passive_enum is_active=UVM_ACTIVE;
+    
+
+    function new(string name="apb_agent_config");
+      super.new(name);
+    endfunction
+    
+    
+  endclass
