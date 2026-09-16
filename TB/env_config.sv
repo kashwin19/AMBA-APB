9aa6c2b1657b2447e2f628252a07802ac1ed4e60
kashwin M S <kaswin620@gmail.com>
2026-09-16 20:29:11 +0530

    Add apb_env_config class for UVM environment

--- TB/env_config.sv
+++ TB/env_config.sv
@@ -1,1 +1,17 @@
 
+
+
+class apb_env_config extends uvm_object;
+  `uvm_object_utils(apb_env_config)
+  
+  function new(string name="apb_env_config");
+    super.new(name);
+  endfunction
+  
+  virtual apb_if vif;
+  
+  apb_agent_config agent_cfg;
+  bit is_scoreboard=1;
+  bit is_coverage=1;
+  
+endclass
