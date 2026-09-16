bc399d59476e33932eb94da3bcb61c50b93ad89d
kashwin M S <kaswin620@gmail.com>
2026-09-16 20:31:31 +0530

    Add APB package with multiple includes

--- TB/package.sv
+++ TB/package.sv
@@ -1,1 +1,25 @@
 
+// `ifndef APB_PKG_SV
+// `define APB_PKG_SV
+
+package apb_package;
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+`include "apb_agent_config.sv"
+`include "apb_env_config.sv"
+
+`include "apb_seq_item.sv"
+`include "apb_sequence.sv"
+`include "apb_sequencer.sv"
+`include "apb_driver.sv"
+`include "apb_monitor.sv"
+`include "apb_agent.sv"
+`include "apb_scoreboard.sv"
+`include "apb_coverage.sv"
+`include "apb_env.sv"
+`include "apb_test.sv"
+endpackage
+
+// `endif
