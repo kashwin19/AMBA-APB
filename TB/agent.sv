dd6d085b94b236955e8c1ded5c8b6f6ed25ab892
kashwin M S <kaswin620@gmail.com>
2026-09-16 20:28:32 +0530

    Implement apb_agent class with build and connect phases

--- TB/agent.sv
+++ TB/agent.sv
@@ -1,1 +1,41 @@
 
+
+  
+  class apb_agent extends uvm_component;
+    `uvm_component_utils(apb_agent)
+    
+    apb_sequencer seqr;
+    apb_driver  drv;
+    apb_monitor mon;
+    
+    apb_agent_config cfg;
+    
+    function new(string name="apb_agent",uvm_component parent);
+      super.new(name,parent);
+      endfunction
+    
+    function void build_phase(uvm_phase phase);
+      super.build_phase(phase);
+      
+      if(!uvm_config_db#(apb_agent_config)::get(this,"","cfg",cfg))
+    `uvm_fatal("AGENT", "CFG not found")
+      
+      mon=apb_monitor::type_id::create("mon",this);
+      
+      if(cfg.is_active==UVM_ACTIVE)begin
+        drv=apb_driver::type_id::create("drv",this);
+        seqr=apb_sequencer::type_id::create("seqr",this);
+      end
+    endfunction
+    
+    function void connect_phase(uvm_phase phase);
+      super.connect_phase(phase);
+      
+      if(cfg.is_active==UVM_ACTIVE)begin
+        drv.seq_item_port.connect(seqr.seq_item_export);
+      end
+    endfunction
+    
+    
+      
+  endclass
